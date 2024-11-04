a045295c994bdddee63238d6506e634754b674f7
alexquicommit <alexis.jeanjean@ynov.com>
2024-11-04 11:40:21 +0100

    init

---
+++ src/test/java/com/locngo/LocngoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locngo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocngoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
