086387001236b8400d6e4ce008923cd98c81520f
alexikicommit <alexis.jeanjean@ynov.com>
2024-11-27 10:10:50 +0100

    feat(liquibasing): Users

--- src/main/java/com/locngo/entity/User.java
+++ src/main/java/com/locngo/entity/User.java
@@ -1,6 +1,8 @@
 package com.locngo.entity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
@@ -8,18 +10,19 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private String name;
+    private String lastname;
     private String firstname;
     private String email;
+    private String phone;
     private String password;
 
     @OneToMany(mappedBy = "user")
     private List<UserRoleMapping> roles;
-
-    // Getters and Setters
 }
